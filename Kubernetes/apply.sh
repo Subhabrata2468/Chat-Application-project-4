89c48cc83905786d05b6c23a7cef3fdf6f9dc81d
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-07-04 09:22:13 +0530

    Create apply.sh

---
+++ Kubernetes/apply.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+kubectl apply -f mongo-deployment.yml
+kubectl apply -f mongo-service.yml
+kubectl apply -f hpa-mongo.yml
+kubectl apply -f backend-deployment.yml
+kubectl apply -f backend-service.yml
+kubectl apply -f hpa-backend.yml
+kubectl apply -f frontend-deployment.yml
+kubectl apply -f frontend-service.yml
+kubectl apply -f hpa-frontend.yml
+kubectl apply -f nodeport.yml
