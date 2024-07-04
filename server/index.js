d2bfa437021b3bd5f63fb26450cec9a7b8bad6fd
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-07-04 21:36:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://frontend-deployment:3000",
+    origin: "http://frontend-service:3000",
     credentials: true,
   },
 });
