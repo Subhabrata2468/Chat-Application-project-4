598b32bb4a8d4373b4ecf2d6910036050d277464
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-07-12 19:57:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://frontend-service.snappy.svc.cluster.local:3000",
+    origin: "http://snappy.com",
     credentials: true,
   },
 });
