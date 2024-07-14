ff3efd39e4a3cf0de1edef9fd5cd75af473ff6f9
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-07-14 19:38:13 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://ec2-3-223-3-141.compute-1.amazonaws.com";
+export const host = "http://ec2-44-210-133-220.compute-1.amazonaws.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
