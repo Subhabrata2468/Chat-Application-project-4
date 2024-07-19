6ecb50c07b96230ccf56d122e2e21524db84208d
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-07-20 03:12:29 +0530

    Update index.js

--- public/src/index.js
+++ public/src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import { initSocket } from './services/socket';
-
-// Initialize WebSocket connection
-initSocket();
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
