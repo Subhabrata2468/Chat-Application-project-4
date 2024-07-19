9ef68122a3dbe7d2a41d3ab97016d2497caa02ca
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-07-20 01:01:43 +0530

    Update App.js

--- public/src/App.js
+++ public/src/App.js
@@ -4,15 +4,25 @@ import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { sendMessage } from './services/socket';
+
 export default function App() {
+  const handleClick = () => {
+    sendMessage('Hello, WebSocket!');
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/setAvatar" element={<SetAvatar />} />
-        <Route path="/" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>WebSocket Example</h1>
+      <button onClick={handleClick}>Send Message</button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/setAvatar" element={<SetAvatar />} />
+          <Route path="/" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
