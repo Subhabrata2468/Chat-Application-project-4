cce17dfdb00a3b55342c04b240491591ed13a171
SUBHABRATA PANDA <126705074+Subhabrata2468@users.noreply.github.com>
2024-07-20 19:09:14 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://54.173.41.171:30980";
+export const host = "http://44.203.31.86:30980";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
